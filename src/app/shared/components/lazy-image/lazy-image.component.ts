b9db03e0e5eff897c05e720849383c26a299a496
Escobar <escobar@matias.com>
2023-06-28 18:28:15 -0300

    Agregado build a docs

--- src/app/shared/components/lazy-image/lazy-image.component.ts
+++ src/app/shared/components/lazy-image/lazy-image.component.ts
@@ -1,9 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'shared-lazy-image',
   templateUrl: './lazy-image.component.html'
 })
-export class LazyImageComponent {
+export class LazyImageComponent implements OnInit {
+
+  @Input()
+  public url!:string;
+
+  @Input()
+  public alt:string = '';
+
+  @Input()
+  public heightImg!: string;
+
+  @Input()
+  public widthImg!: string;
+
+  public hasLoaded: boolean = false;
+
+
+  ngOnInit(): void {
+    if(!this.url) throw new Error('URL property is required');
+  }
+
+  height():number{
+
+    let relationImg:number = parseInt(this.widthImg)/parseInt(this.heightImg);
+
+    return (relationImg);
+  }
+
+  onLoad(){
+    console.log('Image Loaded');
+    this.hasLoaded = true;
+  }
 
 }
